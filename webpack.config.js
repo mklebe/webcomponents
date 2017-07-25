1cdf1c6d5fa34fac42e0ecb6f79cd80f3fe933a4
mklebe <matthias.klebe@gmail.com>
2017-07-25 23:42:59 +0200

    Hope this does not destroy everything

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 var path = require('path');
 var LiveReloadPlugin = require('webpack-livereload-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './app/index.js',
+  entry: [
+      './app/index.js',
+      './components/mk-section/mk-section.sass'
+      ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-      new LiveReloadPlugin()
+      new LiveReloadPlugin(),
+      new ExtractTextPlugin({ // define where to save the file
+        filename: 'dist/[name].bundle.css',
+        allChunks: true,
+      }),
   ],
   module: {
     rules: [{
@@ -18,11 +26,31 @@ module.exports = {
             options: {
             minimize: true
             }
-        }],
+        },
+        { // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader?importLoaders=1',
+        }),
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }],
     },
     {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+    },
+    {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
         }]
+    }]
   }
 };
