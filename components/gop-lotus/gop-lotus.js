1c2556e26b30c2d62048130bd189dd5fe8c5e2e9
mklebe <matthias.klebe@gmail.com>
2017-08-12 00:08:10 +0200

    Implemented bel for programmatic templating and implemented web component as es6 module

---
+++ components/gop-lotus/gop-lotus.js
@@ -0,0 +1,39 @@
+import bel from 'bel'
+let subheadline = 'Its really cool!!'
+let myMarkup = bel([`
+    <div>
+        <h1>bel in web components ROCKS!!</h1>
+        <h2>${subheadline}</h2>
+    </div>
+`])
+
+let template = document.createDocumentFragment();
+
+template.content = myMarkup;
+
+class GOPLotus extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    createdCallback() {
+        let shadow = this.createShadowRoot();
+        window.console.log( template, template.content )
+        let instance = document.importNode(template.content, true);
+        shadow.appendChild(instance);
+    }
+
+    attachedCallback() {
+        window.console.log('GOPLotus is attached')
+    }
+
+    attributeChangedCallback(attrName, oldValue, newValue) {
+        subheadline = newValue;
+    }
+
+    connectedCallback() {
+        window.console.log('GOPLotus connected')
+    }
+}
+
+document.registerElement('gop-lotus', GOPLotus)
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,18 @@
 var path = require('path');
-var LiveReloadPlugin = require('webpack-livereload-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: [
-      './app/index.js',
-      './components/mk-section/mk-section.sass'
-      ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-      new LiveReloadPlugin(),
-      new ExtractTextPlugin({ // define where to save the file
-        filename: 'dist/[name].bundle.css',
-        allChunks: true,
-      }),
-  ],
-  module: {
-    rules: [{
-        test: /\.html$/,
-        use: [ {
-            loader: 'html-loader',
-            options: {
-            minimize: true
-            }
-        },
-        { // regular css files
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          loader: 'css-loader?importLoaders=1',
-        }),
-      },
-      { // sass / scss loader for webpack
-        test: /\.(sass|scss)$/,
-        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-      }],
+    entry: {
+        'gop-lotus': './components/gop-lotus/gop-lotus.js'
     },
-    {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
     },
-    {
-        test: /\.scss$/,
-        use: [{
-            loader: "style-loader" // creates style nodes from JS strings
-        }, {
-            loader: "css-loader" // translates CSS into CommonJS
-        }, {
-            loader: "sass-loader" // compiles Sass to CSS
-        }]
-    }]
-  }
+    plugins: [
+
+    ],
+    module: {
+        rules: [
+        ]
+    }
 };
